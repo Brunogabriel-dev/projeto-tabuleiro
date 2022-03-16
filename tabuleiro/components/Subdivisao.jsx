130148180c2fc47c00f47485006e73841110a98c
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-16 20:50:40 -0300

    Inclusão de texto

--- tabuleiro/components/Subdivisao.jsx
+++ tabuleiro/components/Subdivisao.jsx
@@ -2,6 +2,12 @@ import styles from '../styles/Subdivisao.module.css'
 
 export default function Subdivisao(props) {
   return ( 
-     <div className={styles.subdivisao}></div>
+     <div
+        style={{
+          backgroundColor: props.preta ? "#000" : "#fff"
+        }}
+     className={styles.subdivisao}>
+
+     </div>
   )
 }
--- tabuleiro/pages/index.jsx
+++ tabuleiro/pages/index.jsx
@@ -4,7 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Subdivisao preta/>
       <Subdivisao />
+      <Subdivisao preta/>
     </div>
   )
 }
